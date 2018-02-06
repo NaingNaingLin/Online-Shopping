b04d86c5c926920d738cf38339d24eabd3133288
NaingNaingLin <n.nainglynn@gmail.com>
2018-02-06 16:36:23 +0630

    commit By NNL

--- src/main/java/com/mos/domain/FieldMaster.java
+++ src/main/java/com/mos/domain/FieldMaster.java
@@ -1,5 +1,89 @@
 package com.mos.domain;
 
-public class FieldMaster {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class FieldMaster implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@NotEmpty (message = "Please provide Field Code!")
+	private String fieldCode;
+	
+	@NotEmpty (message = "Please provide Field Name!")
+	private String fieldName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFieldCode() {
+		return fieldCode;
+	}
+
+	public void setFieldCode(String fieldCode) {
+		this.fieldCode = fieldCode;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fieldCode == null) ? 0 : fieldCode.hashCode());
+		result = prime * result + ((fieldName == null) ? 0 : fieldName.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FieldMaster other = (FieldMaster) obj;
+		if (fieldCode == null) {
+			if (other.fieldCode != null)
+				return false;
+		} else if (!fieldCode.equals(other.fieldCode))
+			return false;
+		if (fieldName == null) {
+			if (other.fieldName != null)
+				return false;
+		} else if (!fieldName.equals(other.fieldName))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
 }
