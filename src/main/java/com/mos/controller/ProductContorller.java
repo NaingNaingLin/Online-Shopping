e835053dbc101c0899078aba99dabcbcd42ec5e6
pyae23 <34881016+pyae23@users.noreply.github.com>
2018-02-07 16:59:00 +0630

    tested

--- src/main/java/com/mos/controller/ProductContorller.java
+++ src/main/java/com/mos/controller/ProductContorller.java
@@ -1,13 +1,22 @@
 package com.mos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mos.domain.Product;
+import com.mos.service.ProductService;
 
 @Controller
 public class ProductContorller {
-	@RequestMapping("/products")
-	public String productPage() {
-		return "products";
+	@Autowired
+	private ProductService productService;
+	
+	@RequestMapping("/product")
+	public String productPage(Model model) {
+		model.addAttribute("product" , new Product());
+		return "productForm";
 	}
 
 }
--- src/main/java/com/mos/service/impl/ProductServiceImpl.java
+++ src/main/java/com/mos/service/impl/ProductServiceImpl.java
@@ -2,9 +2,16 @@ package com.mos.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.mos.domain.Product;
 import com.mos.service.ProductService;
 @Service
 public class ProductServiceImpl implements ProductService  {
+
+	@Override
+	public void createProduct(Product product) {
+		
+		
+	}
 	
 	
 
